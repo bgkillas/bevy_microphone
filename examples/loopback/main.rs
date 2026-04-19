ad3b54d01570ffbe006399fec2748c3bebc451f6
bgkillas <walhelm@proton.me>
2026-04-19 00:01:42 -0400

    update deps

--- examples/loopback/main.rs
+++ examples/loopback/main.rs
@@ -1,14 +1,18 @@
 use bevy_microphone::{AudioManager, AudioSettings};
 use rodio::buffer::SamplesBuffer;
-use rodio::{OutputStreamBuilder, Sink};
+use rodio::{ChannelCount, DeviceSinkBuilder, Player, SampleRate};
 pub fn main() {
     let audio = AudioSettings::default();
     let sample_rate = audio.sample_rate;
     let mut audio = AudioManager::new(&audio);
-    let stream_handle = OutputStreamBuilder::open_default_stream().unwrap();
-    let sink = Sink::connect_new(stream_handle.mixer());
+    let stream_handle = DeviceSinkBuilder::open_default_sink().unwrap();
+    let sink = Player::connect_new(stream_handle.mixer());
     audio.recv_audio_decode(|data| {
-        let source = SamplesBuffer::new(1, (sample_rate.get_number() * 1000) as u32, data);
+        let source = SamplesBuffer::new(
+            ChannelCount::new(1).unwrap(),
+            SampleRate::new((sample_rate.get_number() * 1000) as u32).unwrap(),
+            data,
+        );
         sink.append(source);
         sink.play()
     });
